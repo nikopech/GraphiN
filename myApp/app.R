f0c23ae1a813110fc591d412389db460381b7dcb
PKallin <pgkallinik@gmail.com>
2019-08-27 11:25:44 +0300

    ns Untracked files:

--- myApp/app.R
+++ myApp/app.R
@@ -1,3 +1,9 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install()
+
+
+
 library(dplyr)
 library(shiny) 
 library(stringdist)
@@ -7,12 +13,15 @@ library(CINNA)
 library(DT)
 library(ggraph)
 library(graphlayouts)
+library(cluster)
+
 source('C:/Users/Paul Kallin/Desktop/myApp/stringdistances.R', echo=TRUE)
 source('C:/Users/Paul Kallin/Desktop/myApp/visualisation.R', echo=TRUE)
 source('C:/Users/Paul Kallin/Desktop/myApp/filtered.R', echo=TRUE)
 source('C:/Users/Paul Kallin/Desktop/myApp/mstClustering.R', echo=TRUE)
 source('C:/Users/Paul Kallin/Desktop/myApp/multiple_clustering.R', echo=TRUE)
 source('C:/Users/Paul Kallin/Desktop/myApp/visual extended.R', echo=TRUE)
+source('C:/Users/Paul Kallin/Desktop/myApp/clusterMetrics.R', echo=TRUE)
 # Run the application 
 shinyApp(ui = ui, server = server)
 
